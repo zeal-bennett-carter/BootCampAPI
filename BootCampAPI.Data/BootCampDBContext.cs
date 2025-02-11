e601304f26f5541329a6d36af9b16b97385d289a
zeal-bennett-carter <bennett.carter@zealitconsultants.com>
2025-02-11 11:06:06 -0600

    added idatabase and started dbcontext

---
+++ BootCampAPI.Data/BootCampDBContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BootCampAPI.Data.Entities;
+
+namespace BootCampAPI.Data
+{
+    public class BootCampDBContext: DbContext, IDatabase
+    {
+        public BootCampDBContext()
+        {
+        }
+
+        public BootCampDBContext(DbContextOptions<BootCampDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AuthorEntity> Authors { get; set; }
+        public DbSet<BookEntity> Books { get; set; }
+        public DbSet<BookSeriesEntity> BookSeries { get; set; }
+
+
+        public void Migrate()
+            => base.Database.Migrate();
+    }
+}
