8880e621b68c6740011062d2627f0cbbd0eaf562
zeal-bennett-carter <bennett.carter@zealitconsultants.com>
2025-02-24 11:58:53 -0600

    Added DTO objects

---
+++ BootCampAPI.Application/Models/DTO/BookDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootCampAPI.Application.Models.DTO
+{
+    public class BookDTO
+    {
+        public int BookId { get; set; }
+        public int AuthorID { get; set; }
+        public int? BookSeriesID { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+        public string Genre { get; set; }
+        public string Description { get; set; }
+        public int PageCount { get; set; }
+        public int PagesRead { get; set; }
+        public string Publisher { get; set; }
+        public string BookSeriesName { get; set; }
+    }
+}
---
+++ BootCampAPI.Application/Models/DTO/BookSeriesDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootCampAPI.Application.Models.DTO
+{
+    public class BookSeriesDTO
+    {
+        public int BookSeriesId { get; set; }
+        public int AuthorId { get; set; }
+        public string AuthorName { get; set; }
+        public List<int> BooksInSeriesIDs { get; set; }
+        public List<string> BooksInSeriesTitles { get; set; }
+    }
+}
