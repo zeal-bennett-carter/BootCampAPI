fd6be045415a7ed631cd6d99643dd27a79a4b744
zeal-bennett-carter <bennett.carter@zealitconsultants.com>
2025-03-25 16:20:10 -0500

    added edit age endpoint

---
+++ BootCampAPI.Application/DomainEventHandlers/Authors/AgeChanged/LogMessage.cs
@@ -0,0 +1,20 @@
+﻿using BootCampAPI.Application.Notifications;
+using BootCampAPI.Domain.Models.AuthorContent;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootCampAPI.Application.DomainEventHandlers.Authors.AgeChanged
+{
+    public class LogMessage: NotificationHandlerBase<AgeChangedDomainEvent>
+    {
+        protected override Task HandleNotification(Notification<AgeChangedDomainEvent> notification, CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"Author's age has been changed to {notification.Detail.Source.Age}");
+            return Task.CompletedTask;
+        }
+    }
+}
